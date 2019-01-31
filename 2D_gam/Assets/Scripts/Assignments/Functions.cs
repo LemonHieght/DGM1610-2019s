8962a59201d3f865ae79c4a93bfbcc4ec26e9937
will <will.e.lemmons@gmail.com>
2019-01-30 17:01:57 -0700

    learn functions

---
+++ 2D_gam/Assets/Scripts/Assignments/Functions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : MonoBehaviour {
+	public string weather;
+	
+	void Start(){
+		Weather(weather);
+	}
+	void Weather (string weatherState){
+		
+		if(weatherState == "Sunny"){
+			print("The sun is shinning today!");
+		}
+		else if(weatherState == "Raining"){
+			print("It's soggy and wet today.");
+		}
+		else if(weatherState == "Windy"){
+			print("It's Very windy today.");
+		}
+		else if(weatherState == "Snowy"){
+			print("It's mighty snowy today.");
+		}
+		else if(weatherState == "Foggy"){
+			print("Visability is poor do to fog.");
+		}
+		else{
+			print ("I don't understand "+ weatherState);
+		}
+	}
+}
