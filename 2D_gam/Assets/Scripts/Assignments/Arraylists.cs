78b44ee2722df36cecaf75ee4e92bf9a19084652
Will <will.e.lemmons@gmail.com>
2019-03-10 21:57:51 -0600

    octo, array, and lists

---
+++ 2D_gam/Assets/Scripts/Assignments/Arraylists.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arraylists : MonoBehaviour
+{
+    public string client1 = "Zook";
+    public string client2 = "Mulch";
+    public string client3 = "Nott";
+    public string client4 = "Grog";
+    public string client5 = "Fairien";
+   
+    //Array Ex.
+    public string[] clientList = new string[]{"Zook","Grog","Nott","Fairien","Mulch"};
+   
+    //List Ex.
+    public List<string> santaList = new List<string>();
+
+    //Array List Ex.
+    public ArrayList inventory = new ArrayList();
+   
+    // use this to Initiallization
+    void Start()
+    {
+        santaList.Add("Caleb");
+        santaList.Add("Grim");
+        santaList.Add("Fjord");
+        santaList.Add("Clay");
+        santaList.Add("Bou");
+        
+        santaList.Remove("Caleb");
+
+        print(clientList[4]);
+        print(clientList[2]);
+
+        inventory.Add(23);
+        inventory.Add("Jaw Bone");
+        inventory.Add(true);
+        inventory.Add(3.14159);
+
+        print(inventory[3]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2D_gam/Assets/Scripts/Assignments/Switch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public string suspect;
+    public string weapon;
+    public string room;
+    public string victim;
+
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        murderMystery(suspect, weapon);
+    }
+    void murderMystery(string person, string weapon)
+    {
+        switch(person)
+        {
+            case "Mulch":
+                print("Where's my Lawer!  I won't Talk until he's here.");
+                break;
+            case "Zonphone":
+                print("Bro, why the harsh mellow.  You know I was with Mulch in the game room, Brah.");
+                break;
+            case "Philbert":
+                print("I do say Detective, I was in the shower when you all got here.");
+                break;
+            case "ItsMe":
+                print("I don't Know who's done it, but I did see Phibert leaving the kitchen at the time.");
+                break;
+            case "Sherlly":
+                print("What's happening?  All i know is I was cooking and then the police suddenly came knocking on the door.");
+                break;
+            default:
+                print("I'm not familiar with "+suspect+".");
+                break;
+        }
+    }
+}
