d67f215c950527626e288a430420a3a37ed97783
Will <will.e.lemmons@gmail.com>
2019-03-13 18:22:07 -0600

    main menu

---
+++ 2D_gam/Assets/Scripts/Game/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Indexed Level to load
+    public int levelToLoad;
+
+//this function loads the level in the levelToLoad var after button in pressed
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+    //this fuction quits game after the button is pressed
+    public void LevelExit()
+    {
+        Application.Quit();
+    }
+
+}
