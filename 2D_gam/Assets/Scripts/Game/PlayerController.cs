6419a22fa770f0f86be245674b7ce1589c9c6f94
Will <will.e.lemmons@gmail.com>
2019-04-04 14:25:16 -0600

    Start Animation

--- 2D_gam/Assets/Scripts/Game/PlayerController.cs
+++ 2D_gam/Assets/Scripts/Game/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
-
+    public float walkSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -20,9 +20,17 @@ public class PlayerController : MonoBehaviour
         {
             transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
         }
+        else if(Input.GetAxisRaw("Horizontal") < 0.5f || Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
+            transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * walkSpeed * Time.deltaTime, 0f, 0f));
+        }
         if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
             transform.Translate ( new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+        }
+        else if(Input.GetAxisRaw("Vertical") < 0.5f || Input.GetAxisRaw("Vertical") > -0.5f)
+        {
+            transform.Translate ( new Vector3(0f, Input.GetAxisRaw("Vertical") * walkSpeed * Time.deltaTime, 0f));
         }
     }
 }
