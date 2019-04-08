b8c523fd5bdc3f475b80cfa6bfe533cac01a99c2
Will <will.e.lemmons@gmail.com>
2019-04-08 17:19:43 -0600

    WorldGenFail

---
+++ 2D_gam/Assets/Scripts/Game/MapGen.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGen : MonoBehaviour
+{
+    public int width, height;
+    public GameObject tileGrass;
+    public int tileOffSet = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenMap();
+    }
+    void GenMap()
+    {
+        for(int x = -1; x < width; x++)
+        {
+            for(int y = -1; y < height ; y++)
+            {
+                Vector3 position = new Vector3(x + tileOffSet, y + tileOffSet, 0);
+                Instantiate(tileGrass, position, Quaternion.identity);
+            }
+            
+        }
+        for(int x = 1; x > width; x++)
+        {
+            for(int y = 1; y > height ; y++)
+            {
+                Vector3 position = new Vector3(x - tileOffSet, y - tileOffSet, 0);
+                Instantiate(tileGrass, position, Quaternion.identity);
+            }
+            
+        }
+
+    }
+}
