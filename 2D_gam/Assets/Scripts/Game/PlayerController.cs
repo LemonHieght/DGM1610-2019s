4a9d3ba061638d696fc881fe1c9c566f4879facc
Will <will.e.lemmons@gmail.com>
2019-04-13 10:42:08 -0600

    Clean Move

--- 2D_gam/Assets/Scripts/Game/PlayerController.cs
+++ 2D_gam/Assets/Scripts/Game/PlayerController.cs
@@ -6,11 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float walkSpeed;
+    private Rigidbody2D myRigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -18,19 +19,35 @@ public class PlayerController : MonoBehaviour
     {
         if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
-            transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            // transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
+
         }
         else if(Input.GetAxisRaw("Horizontal") < 0.5f || Input.GetAxisRaw("Horizontal") > -0.5f)
         {
-            transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * walkSpeed * Time.deltaTime, 0f, 0f));
+            // transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * walkSpeed * Time.deltaTime, 0f, 0f));
+             myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * walkSpeed, myRigidbody.velocity.y);
+
         }
         if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
-            transform.Translate ( new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            // transform.Translate ( new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+             myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+
         }
         else if(Input.GetAxisRaw("Vertical") < 0.5f || Input.GetAxisRaw("Vertical") > -0.5f)
         {
-            transform.Translate ( new Vector3(0f, Input.GetAxisRaw("Vertical") * walkSpeed * Time.deltaTime, 0f));
+            // transform.Translate ( new Vector3(0f, Input.GetAxisRaw("Vertical") * walkSpeed * Time.deltaTime, 0f));
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * walkSpeed);
+
+        }
+        if(Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
+            myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y);
+        }
+        if(Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
         }
     }
 }
