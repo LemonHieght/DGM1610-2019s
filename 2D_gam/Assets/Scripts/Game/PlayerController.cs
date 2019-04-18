a7cacb5af23babc7f6aa4cf31ce04db53ced850e
Will <will.e.lemmons@gmail.com>
2019-04-18 13:04:11 -0600

    attack animation

--- 2D_gam/Assets/Scripts/Game/PlayerController.cs
+++ 2D_gam/Assets/Scripts/Game/PlayerController.cs
@@ -10,6 +10,9 @@ public class PlayerController : MonoBehaviour
     private Animator anim;
     private bool playerMove;
     private Vector2 lastMove;
+    private bool attack;
+    public float attackTime;
+    private float attackTimeCounter;
 
     // Start is called before the first frame update
     void Start()
@@ -23,37 +26,49 @@ public class PlayerController : MonoBehaviour
     {
         
         playerMove = false;
-        
-        //walking and movement
-        
-        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        if(!attack)
         {
-            // transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
-            myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
-            playerMove = true;
-            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+            
+            //walking and movement
+        
+            if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+            {
+                // transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+                myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
+                playerMove = true;
+                lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
 
+            }
+        
+            if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+            {
+                // transform.Translate ( new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+                playerMove = true;
+                lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+
+            }
+        
+            //To stop ice skate sliding
+
+            if(Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+            {
+                myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y);
+            }
+            if(Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+
+            attackTimeCounter = attackTime;
+            attack = true;
+            myRigidbody.velocity = Vector2.zero;
+            anim.SetBool("Attack", true);
         }
         
-        if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
-        {
-            // transform.Translate ( new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-             myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
-            playerMove = true;
-            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
-
-        }
-        
-        //To stop ice skate sliding
-
-        if(Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
-        {
-            myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y);
-        }
-        if(Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
-        {
-            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
-        }
         //Animation transition
 
         anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
