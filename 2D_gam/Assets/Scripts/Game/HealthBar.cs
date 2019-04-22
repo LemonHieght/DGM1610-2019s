c3d1cfd9357cc25ee1f839be2309719239347a7b
Will <will.e.lemmons@gmail.com>
2019-04-21 20:46:24 -0600

    Health bar

---
+++ 2D_gam/Assets/Scripts/Game/HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider health;
+    public Text healthCount;
+    public PlayerHealth playerHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health.maxValue = playerHealth.playerMaxHealth;
+        health.value = playerHealth.playerCurrentHealth;
+        healthCount.text = "Health: " + playerHealth.playerCurrentHealth;
+    }
+}
