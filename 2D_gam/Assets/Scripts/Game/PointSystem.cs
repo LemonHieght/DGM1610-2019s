b04ecd8ec0e908c7bdcbc4777f65376255ea7b3b
Will <will.e.lemmons@gmail.com>
2019-04-23 13:15:48 -0600

    score

--- 2D_gam/Assets/Scripts/Game/PointSystem.cs
+++ 2D_gam/Assets/Scripts/Game/PointSystem.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PointSystem : MonoBehaviour
 {
+    public static int score;
+    public Text scoreText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText = GetComponent<Text>();
+        score = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(score < 0)
+        {
+            score = 0;
+        }
     }
 }
