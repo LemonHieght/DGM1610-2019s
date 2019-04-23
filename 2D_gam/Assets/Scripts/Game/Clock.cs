2209bf5c3d5d55b304b9c9cabc5e9576928cd126
Will <will.e.lemmons@gmail.com>
2019-04-23 12:50:38 -0600

    spawn system

---
+++ 2D_gam/Assets/Scripts/Game/Clock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public float currentTime;
+    public float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+    }
+}
