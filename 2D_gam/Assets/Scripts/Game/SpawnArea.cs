f1b036894be1ac478b12b82288c331dac3919ea2
Will <will.e.lemmons@gmail.com>
2019-04-23 21:56:21 -0600

    spawn Amount

--- 2D_gam/Assets/Scripts/Game/SpawnArea.cs
+++ 2D_gam/Assets/Scripts/Game/SpawnArea.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnArea : MonoBehaviour
 {
     public int spawnAmount;
+    public float spawnTime;
     public float spawnTimeCounter;
     public GameObject enemy;
     public Vector3 center;
@@ -28,15 +29,18 @@ public class SpawnArea : MonoBehaviour
         {
             SpawnEnemy();
             spawnAmount += 2;
-            spawnTimeCounter = 5;
+            spawnTimeCounter = spawnTime;
 
         }
     }
 
     public void SpawnEnemy()
     {
-        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x /2), Random.Range(-size.y / 2, size.y / 2), 0f);
-        Instantiate(enemy , pos, Quaternion.identity);
+        for(int i = 0; i < spawnAmount; i++)
+        {
+            Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x /2), Random.Range(-size.y / 2, size.y / 2), 0f);
+            Instantiate(enemy , pos, Quaternion.identity);
+        }
         
     }
 
