d863b3bb7703a8020d74dd947faac2f58ea64c33
Will Lemmons <will.e.lemmons@gmail.com>
2021-04-30 09:18:35 -0800

    Update2

--- 2D_gam/Assets/Scripts/Game/PlayerAttack.cs
+++ 2D_gam/Assets/Scripts/Game/PlayerAttack.cs
@@ -19,7 +19,7 @@ public class PlayerAttack : MonoBehaviour
     
     }
 
-    void OnTriggerStay2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
 
         if(other.gameObject.tag == "Enemy")
